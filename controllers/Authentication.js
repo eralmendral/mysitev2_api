e55d2547a55f72a6312107f06adc0d27a2145737
eralmendral <eralmendral@gmail.com>
2021-01-07 21:05:16 +0800

    use jwt-simple library to generate a jwtoken

--- controllers/Authentication.js
+++ controllers/Authentication.js
@@ -1,10 +1,16 @@
+const jwt = require('jwt-simple');
 const User = require('../models/User');
+
+function generateToken(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id , iat: timestamp }, process.env.SECRET);   // sub = subject,   iat = issued at time
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
   if (!email || !password) return res.status(422).send({ error: 'Please provide email and password.' });
-  
+
   // see if email already taken;
   User.findOne({ email: email }, function (err, existingUser) {
     if (err) return next(err);
@@ -21,7 +27,7 @@ exports.signup = function (req, res, next) {
     user.save(function (err) {
       if (err) return next(err);
 
-      return res.json(user);
+      return res.json({ token: generateToken(user) });
     });
 
   });
