5378221ad89765f5add3195b7e4f0c60d84c9f00
eralmendral <eralmendral@gmail.com>
2021-01-07 20:10:11 +0800

    initial route handler

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+// App setup
+app.use(morgan('combined'));
+app.use(express.json()); //Used to parse JSON bodies
+// app.use(express.urlencoded()); //Parse URL-encoded bodies
 
+// Routes
+router(app);
+
+// Server setup
 const port = process.env.PORT || 5000;
-
 app.listen(port, (req, res) => {
   console.log('Server running..');
 });
