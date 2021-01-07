242ea7ea39b02e48e290f7598390bf30c0acaf1e
eralmendral <eralmendral@gmail.com>
2021-01-07 20:25:22 +0800

    setup connection to mongodb

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const dbConnect  = require('./config/db');
+
+// Configs
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env'})
+
+
 // App setup
 app.use(morgan('combined'));
 app.use(express.json()); //Used to parse JSON bodies
-// app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies'
+
+// DB setup
+dbConnect();
 
 // Routes
 router(app);
